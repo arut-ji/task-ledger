92340f871ee62ca259f200092c4c5755e4a3f36a
Pasuta Paopun <60090025@kmitl.ac.th>
2019-04-20 10:18:49 +0700

    added new file and refactor code

---
+++ client/Code/schedule.py
@@ -0,0 +1,21 @@
+import sys
+
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+from client.Code.ui_to_py.schedule_ui import Ui_Form
+
+class scheduleUI(QWidget):
+    def __init__(self):
+        QWidget.__init__(self, None)
+
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+
+        self.show()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    w = scheduleUI()
+    sys.exit(app.exec_())
+

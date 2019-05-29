2bf75b2fd9ddc4f9619690fdb6d5f0a025c81f60
Pasuta Paopun <60090025@kmitl.ac.th>
2019-05-29 18:59:20 +0700

    edited code

--- client/Code/ui_to_py/schedule_ui.py
+++ client/Code/ui_to_py/schedule_ui.py
@@ -1,7 +1,11 @@
-from PySide2 import QtWidgets, QtCore, QtGui
-from PySide2.QtCore import QSize
-from PySide2.QtGui import QIcon
+import sys
 
+from PySide2 import QtWidgets, QtCore, QtGui
+from PySide2.QtCore import QSize, Qt
+from PySide2.QtGui import QIcon
+import datetime
+
+from PySide2.QtWidgets import QApplication
 
 mockTaskList = [
     {
@@ -32,9 +36,9 @@ class Schedule_ui(QtWidgets.QWidget):
     def __init__(self, task_list=None, parent=None):
         super(Schedule_ui, self).__init__(parent)
         self.tasks = task_list
-        self.date = QtCore.QDate()
+        self.date_now = datetime.date.today()
 
-    def setupUi(self, parent):
+    def setupUi(self, parent=None):
         # Button
         self.left_arrow = QtWidgets.QPushButton(parent)
         self.left_arrow.setGeometry(QtCore.QRect(30, 55, 41, 41))
@@ -43,6 +47,7 @@ class Schedule_ui(QtWidgets.QWidget):
         self.left_arrow.setFlat(True)
         self.left_arrow.setIconSize(QSize(21, 21))
         self.left_arrow.setObjectName("left_arrow")
+        self.left_arrow.clicked.connect(self.prev_date)
 
         self.right_arrow = QtWidgets.QPushButton(parent)
         self.right_arrow.setGeometry(QtCore.QRect(80, 55, 41, 41))
@@ -51,28 +56,23 @@ class Schedule_ui(QtWidgets.QWidget):
         self.right_arrow.setFlat(True)
         self.right_arrow.setIconSize(QSize(21, 21))
         self.right_arrow.setObjectName("right_arrow")
+        self.right_arrow.clicked.connect(self.next_date)
 
-        print(self.date.currentDate())
-
-        
         self.today_label = QtWidgets.QLabel(parent)
-
-        self.today_label.setGeometry(QtCore.QRect(130, 20, 91, 51))
+        self.today_label.setGeometry(QtCore.QRect(130, 20, 200, 51))
         font = QtGui.QFont()
         font.setFamily("Helvetica Neue")
         font.setPointSize(32)
         self.today_label.setFont(font)
         self.today_label.setObjectName("today_label")
-        self.today_label.setText("Today")
 
         self.date = QtWidgets.QLabel(parent)
-        self.date.setGeometry(QtCore.QRect(120, 60, 291, 51))
+        self.date.setGeometry(QtCore.QRect(120, 60, 500, 51))
         font = QtGui.QFont()
         font.setFamily("Helvetica")
         font.setPointSize(42)
         self.date.setFont(font)
         self.date.setObjectName("label_3")
-        self.date.setText(" Mon 15 April")
 
         self.list_view = QtWidgets.QListView(parent)
         self.list_view.setGeometry(80, 120, 590, 450)
@@ -81,11 +81,13 @@ class Schedule_ui(QtWidgets.QWidget):
         self.list_view.setModel(self.model)
         self.model.clear()
 
-        for task in self.tasks:
-            taskListItem = QtGui.QStandardItem(task['topic'])
-            taskListItem.setCheckable(True)
-            # taskListItem.setCheckState(task['status'])
-            self.model.appendRow(taskListItem)
+        self.update_label(self.date_now)
+
+        # for task in self.tasks:
+        #     taskListItem = QtGui.QStandardItem(task['topic'])
+        #     taskListItem.setCheckable(True)
+        #     # taskListItem.setCheckState(task['status'])
+        #     self.model.appendRow(taskListItem)
 
         # self.checkBox_3 = QtWidgets.QCheckBox(parent)
         # self.checkBox_3.setGeometry(QtCore.QRect(100, 170, 271, 41))
@@ -96,3 +98,25 @@ class Schedule_ui(QtWidgets.QWidget):
         # self.checkBox_4.setGeometry(QtCore.QRect(100, 120, 271, 41))
         # self.checkBox_4.setObjectName("checkBox_4")
         # self.checkBox_4.setText('hi')
+
+    def next_date(self):
+        self.date_now += datetime.timedelta(days=1)
+        self.update_label(self.date_now)
+
+    def prev_date(self):
+        self.date_now -= datetime.timedelta(days=1)
+        self.update_label(self.date_now)
+
+    def update_label(self, date):
+        self.date.setText(self.get_str_date(self.date_now))
+        self.today_label.setText(date.strftime("%A"))
+
+    def get_date(self):
+        return self.date_now
+
+    def set_date(self, date):
+        self.date_now = date
+
+    def get_str_date(self, date):
+        return ' ' + date.strftime("%d") + ' ' \
+               + date.strftime("%B") + ' ' + date.strftime("%Y")

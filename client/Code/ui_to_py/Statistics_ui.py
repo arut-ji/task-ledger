81ef6d7d3b5f2a79ad8d9b1ced0e4631fc49c1c6
Pasuta Paopun <60090025@kmitl.ac.th>
2019-06-01 00:02:06 +0700

    Updated BarChart

--- client/Code/ui_to_py/Statistics_ui.py
+++ client/Code/ui_to_py/Statistics_ui.py
@@ -1,3 +1,4 @@
+import datetime
 import math
 import sys
 from random import randrange
@@ -5,167 +6,17 @@ from random import randrange
 from PySide2.QtCore import QAbstractTableModel, QModelIndex, QRect, Qt, QSize
 from PySide2.QtGui import QColor, QPainter, QFont, QPen, QIcon
 from PySide2.QtWidgets import (QApplication, QHeaderView,
-                               QTableView, QWidget, QVBoxLayout, QLabel, QFrame, QPushButton)
+                               QTableView, QWidget, QVBoxLayout, QLabel, QFrame, QPushButton, QComboBox)
 from PySide2.QtCharts import QtCharts
 
+from client.Code.controller.models.models import TaskList
 from client.Code.ui_to_py.bar_chart import BarChart
-
-
-class CustomTableModel(QAbstractTableModel):
-    def __init__(self):
-        QAbstractTableModel.__init__(self)
-        self.input_data = []
-        self.mapping = {}
-        self.column_count = 2
-        self.row_count = 7
-        self.min_val = math.inf
-        self.max_val = -math.inf
-
-        for i in range(self.row_count):
-            data_vec = [0]*self.column_count
-            for k in range(len(data_vec)):
-                if k == 0:
-                    data_vec[k] = i + 1
-                if k == 1:
-                    data_vec[k] = randrange(100)
-                    self.min_val = min(self.min_val, data_vec[k])
-                    self.max_val = max(self.max_val, data_vec[k])
-            self.input_data.append(data_vec)
-        print(self.input_data)
-
-    def rowCount(self, parent=QModelIndex()):
-        return len(self.input_data)
-
-    def columnCount(self, parent=QModelIndex()):
-        return self.column_count
-
-    def headerData(self, section, orientation, role):
-        if role != Qt.DisplayRole:
-            return None
-
-        if orientation == Qt.Horizontal:
-            if section % 2 == 0:
-                return "x"
-            else:
-                return "y"
-        else:
-            return "{}".format(section + 1)
-
-    def data(self, index, role=Qt.DisplayRole):
-        if role == Qt.DisplayRole:
-            return self.input_data[index.row()][index.column()]
-        elif role == Qt.EditRole:
-            return self.input_data[index.row()][index.column()]
-        elif role == Qt.BackgroundRole:
-            for color, rect in self.mapping.items():
-                if rect.contains(index.column(), index.row()):
-                    return QColor(color)
-            # cell not mapped return white color
-            return QColor(Qt.white);
-        return None
-
-    def setData(self, index, value, role=Qt.EditRole):
-        if index.isValid() and role == Qt.EditRole:
-            self.input_data[index.row()][index.column()] = float(value)
-            self.dataChanged.emit(index, index)
-            return True
-        return False
-
-    def flags(self, index):
-        return Qt.ItemIsEnabled | Qt.ItemIsEditable | Qt.ItemIsSelectable
-
-    def add_mapping(self, color, area):
-        self.mapping[color] = area
-
-    def clear_mapping(self):
-        self.mapping = {}
-
-
-
-class TableWidget(QWidget):
-    def __init__(self, parent=None):
-        QWidget.__init__(self, parent)
-
-        self.model = CustomTableModel()
-
-        self.table_view = QTableView()
-        self.table_view.setModel(self.model)
-        self.table_view.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
-        self.table_view.verticalHeader().setSectionResizeMode(QHeaderView.Stretch)
-
-        self.chart = QtCharts.QChart()
-        self.chart.setAnimationOptions(QtCharts.QChart.AllAnimations)
-
-        self.series = QtCharts.QLineSeries()
-        pen = QPen()
-        pen.setWidth(3)
-        pen.setColor(QColor(242, 183, 54))
-        pen.setCapStyle(Qt.RoundCap)
-        self.series.setPen(pen)
-        self.series.setName("Weekly Karma Points")
-        self.mapper = QtCharts.QVXYModelMapper(self)
-        self.mapper.setXColumn(0)
-        self.mapper.setYColumn(1)
-        self.mapper.setSeries(self.series)
-        self.mapper.setModel(self.model)
-        self.chart.addSeries(self.series)
-
-        # for storing color hex from the series
-        seriesColorHex = "#000000"
-
-        # get the color of the series and use it for showing the mapped area
-        seriesColorHex = "{}".format(self.series.pen().color().name())
-        self.model.add_mapping(seriesColorHex, QRect(0, 0, 2, self.model.rowCount()))
-
-        # series 2
-        # self.series = QtCharts.QLineSeries()
-        # self.series.setName("Monthy")
-        #
-        # self.mapper = QtCharts.QVXYModelMapper(self)
-        # self.mapper.setXColumn(2)
-        # self.mapper.setYColumn(3)
-        # self.mapper.setSeries(self.series)
-        # self.mapper.setModel(self.model)
-        # self.chart.addSeries(self.series)
-        #
-        # # get the color of the series and use it for showing the mapped area
-        # seriesColorHex = "{}".format(self.series.pen().color().name())
-        # self.model.add_mapping(seriesColorHex, QRect(2, 0, 2, self.model.rowCount()))
-
-        axisX = QtCharts.QCategoryAxis()
-        axisX.append("SUN", 1)
-        axisX.append("MON", 2)
-        axisX.append("TUE", 3)
-        axisX.append("WED", 4)
-        axisX.append("THU", 5)
-        axisX.append("FRI", 6)
-        axisX.append("SAT", 7)
-        axisX.setRange(0, 7)
-
-        axisY = QtCharts.QValueAxis()
-
-        axisY.setRange(self.model.min_val, self.model.max_val)
-
-
-        self.chart.addAxis(axisX, Qt.AlignBottom)
-        self.chart.addAxis(axisY, Qt.AlignLeft)
-        self.chart_view = QtCharts.QChartView(self.chart)
-        self.chart_view.setRenderHint(QPainter.Antialiasing)
-        self.chart_view.setFixedSize(651, 431)
-
-        # self.chart_view.setGeometry(20, 100, 671, 451)
-        # create main layout
-        self.main_layout = QVBoxLayout(self)
-        # self.main_layout.addWidget(self.table_view, 1, 0)
-        self.main_layout.addWidget(self.chart_view)
-        # self.main_layout.setColumnStretch(1, 1)
-        # self.main_layout.setColumnStretch(0, 0)
-        self.setLayout(self.main_layout)
 
 
 class StatisticsUI(QWidget):
     def __init__(self, parent=None):
         super(StatisticsUI, self).__init__(parent)
+        self.task_list = None
 
     def setupUI(self, parent=None):
         self.stat_label = QLabel(parent)
@@ -176,32 +27,54 @@ class StatisticsUI(QWidget):
         self.stat_label.setFont(font)
         self.stat_label.setObjectName("stat_label")
         self.stat_label.setText("Statistics")
-        #
-        # self.left_arrow = QPushButton(parent)
-        # self.left_arrow.setGeometry(QRect(30, 55, 41, 41))
-        # self.left_arrow.setText("")
-        # self.left_arrow.setIcon(QIcon("../Assets/arrow_left.png"))
-        # self.left_arrow.setFlat(True)
-        # self.left_arrow.setIconSize(QSize(21, 21))
-        # self.left_arrow.setObjectName("left_arrow")
-        # self.left_arrow.clicked.connect(self.prev_date)
-
-        # self.right_arrow = QPushButton(parent)
-        # self.right_arrow.setGeometry(QRect(80, 55, 41, 41))
-        # self.right_arrow.setText("")
-        # self.right_arrow.setIcon(QIcon("../Assets/arrow_right.png"))
-        # self.right_arrow.setFlat(True)
-        # self.right_arrow.setIconSize(QSize(21, 21))
-        # self.right_arrow.setObjectName("right_arrow")
-        # self.right_arrow.clicked.connect(self.next_date)
 
         self.stat_frame = QFrame(parent)
         self.stat_frame.setObjectName("stat_frame")
         self.stat_frame.setGeometry(20, 100, 671, 451)
 
-        self.stat_graph = BarChart([0, 1, -1, -9, 2, 3, -2], self.stat_frame)
+        self.stat_graph = BarChart(self.stat_frame)
         self.stat_graph.setGeometry(0, 30, 671, 451)
         self.stat_graph.show()
+
+
+        self.left_arrow = QPushButton(parent)
+        self.left_arrow.setGeometry(QRect(50, 110, 41, 41))
+        self.left_arrow.setText("")
+        self.left_arrow.setIcon(QIcon("../Assets/arrow_left.png"))
+        self.left_arrow.setFlat(True)
+        self.left_arrow.setIconSize(QSize(21, 21))
+        self.left_arrow.setObjectName("left_arrow")
+        self.left_arrow.clicked.connect(self.change_chart_backward)
+
+        self.right_arrow = QPushButton(parent)
+        self.right_arrow.setGeometry(QRect(100, 110, 41, 41))
+        self.right_arrow.setText("")
+        self.right_arrow.setIcon(QIcon("../Assets/arrow_right.png"))
+        self.right_arrow.setFlat(True)
+        self.right_arrow.setIconSize(QSize(21, 21))
+        self.right_arrow.setObjectName("right_arrow")
+        self.right_arrow.clicked.connect(self.change_chart_forward)
+
+        self.combo_box = QComboBox(parent)
+        self.combo_box.setGeometry(QRect(150, 110, 121, 36))
+        self.combo_box.addItem("Weekly")
+        self.combo_box.addItem("Monthly")
+        self.combo_box.setCurrentIndex(0)
+
+        # if self.combo_box.currentData()
+
+    def update_data(self, task_list: TaskList):
+        self.task_list = task_list
+        self.stat_graph.update_chart(task_list, datetime.datetime.now())
+        self.update()
+
+    def change_chart_backward(self):
+        self.stat_graph.change_max_bound(-7)
+        # self.stat_graph.update()
+
+    def change_chart_forward(self):
+        self.stat_graph.change_max_bound(7)
+        # self.stat_graph.update()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- client/Code/ui_to_py/bar_chart.py
+++ client/Code/ui_to_py/bar_chart.py
@@ -2,45 +2,42 @@ import datetime
 import sys
 
 from PySide2.QtCharts import QtCharts
-from PySide2.QtCore import Qt, QDateTime
-from PySide2.QtGui import QPainter
+from PySide2.QtCore import Qt
+from PySide2.QtGui import QPainter, QColor
 from PySide2.QtWidgets import QWidget, QApplication, QVBoxLayout
-from PySide2 import QtCore
+
+from client.Code.controller.models.models import TaskList
 
 
 class BarChart(QWidget):
-    def __init__(self, lst, parent=None):
+    def __init__(self, parent=None):
         QWidget.__init__(self, parent)
-        set_sun = QtCharts.QBarSet("ME")
-        self.lst = lst
+        bar_set = QtCharts.QBarSet("ME")
+        bar_set.setColor(QColor(231, 119, 32))
+        self.task_list = None
+
+        self.max_bound: datetime = None
+        self.min_bound: datetime = None
 
         self.setFixedSize(651, 431)
 
-        for i in range(len(self.lst)):
-            set_sun.append(self.lst[i])
+        # for i in range(len(self.lst)):
+        #     bar_set.append(self.lst[i])
 
         self.series = QtCharts.QBarSeries()
-        self.series.append(set_sun)
+        self.series.append(bar_set)
 
         self.chart = QtCharts.QChart()
         self.chart.addSeries(self.series)
         self.chart.setTitle("Karma Points")
         self.chart.setAnimationOptions(QtCharts.QChart.SeriesAnimations)
 
-        axisX = QtCharts.QBarCategoryAxis()
-        self.date_now = datetime.date.today()
-        print(self.date_to_string(self.date_now - datetime.timedelta(days=6)))
-        axisX.append([self.date_to_string(self.date_now - datetime.timedelta(days=6)),
-                      self.date_to_string(self.date_now - datetime.timedelta(days=5)),
-                      self.date_to_string(self.date_now - datetime.timedelta(days=4)),
-                      self.date_to_string(self.date_now - datetime.timedelta(days=3)),
-                      self.date_to_string(self.date_now - datetime.timedelta(days=2)),
-                      self.date_to_string(self.date_now - datetime.timedelta(days=1)),
-                      self.date_to_string(self.date_now)])
+        self.axisX = QtCharts.QBarCategoryAxis()
+        # self.update_chart(datetime.date.today())
+        # print(self.date_now - datetime.timedelta(days=6))
 
         self.chart.createDefaultAxes()
-        self.chart.setAxisX(axisX, self.series)
-        self.series.attachAxis(axisX)
+        self.chart.setAxisX(self.axisX, self.series)
         self.chart.legend().setVisible(True)
         self.chart.legend().setAlignment(Qt.AlignBottom)
 
@@ -51,12 +48,41 @@ class BarChart(QWidget):
         self.main_layout.addWidget(self.chartView)
         self.setLayout(self.main_layout)
 
+    def set_task_list(self, task_list):
+        self.task_list = task_list
+
+    def set_max_bound(self, max_bound_date):
+        self.max_bound = max_bound_date
+        self.update_chart(task_list=self.task_list)
+
+    def change_max_bound(self, offset: int):
+        if offset > 0:
+            self.max_bound += datetime.timedelta(days=offset)
+        else:
+            self.max_bound -= datetime.timedelta(days=-offset)
+        self.update_chart(self.task_list)
+
+    def update_chart(self, task_list: TaskList, current_date=None):
+        self.axisX.clear()
+        if current_date is not None:
+            self.max_bound = current_date
+
+
+        self.axisX.append([self.date_to_string(self.max_bound - datetime.timedelta(days=6)),
+                           self.date_to_string(self.max_bound - datetime.timedelta(days=5)),
+                           self.date_to_string(self.max_bound - datetime.timedelta(days=4)),
+                           self.date_to_string(self.max_bound - datetime.timedelta(days=3)),
+                           self.date_to_string(self.max_bound - datetime.timedelta(days=2)),
+                           self.date_to_string(self.max_bound - datetime.timedelta(days=1)),
+                           self.date_to_string(self.max_bound)])
+        self.update()
+
     def date_to_string(self, date):
         return date.strftime("%d") + ' ' + date.strftime("%b")
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     w = BarChart([0, 1, -1, -9, 2, 3, -2])
     w.show()
     sys.exit(app.exec_())
-
